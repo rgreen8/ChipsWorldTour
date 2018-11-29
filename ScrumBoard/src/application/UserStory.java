1d1503961b69303cf3651a091c4d2c8501afe8ef
cgreen14 <cgreen14@nd.edu>
2018-11-28 22:01:22 -0500

    DJ

--- ScrumBoard/src/application/UserStory.java
+++ ScrumBoard/src/application/UserStory.java
@@ -2,4 +2,26 @@ package application;
 
 public class UserStory {
 
+	private boolean _LOCKED_;
+
+	public UserStory()
+	{
+	_LOCKED_ = false;
+	}
+
+	public boolean isLocked()
+	{
+	return _LOCKED_;
+	}
+
+	public boolean editStory()
+	{
+	if (!this.isLocked())
+	{
+	// TODO: Send to appropriate "editing handlers"
+	return true;
+	}
+	else
+	return false;
+	}
 }
