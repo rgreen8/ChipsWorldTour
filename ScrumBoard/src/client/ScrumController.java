9d00aa626d550e58ea5426845c3422102f446674
Daniel Burns <danieljburns143@gmail.com>
2018-12-12 23:18:00 -0500

    Feat: Drag n' Drop almost finished

--- ScrumBoard/src/client/ScrumController.java
+++ ScrumBoard/src/client/ScrumController.java
@@ -1,8 +1,12 @@
 package client;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 
@@ -16,6 +20,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -121,6 +128,7 @@ public class ScrumController implements Initializable  {
          Pane newStory = loader2.load();
          storyController storyControl = loader2.getController();
          storyControl.addStory(newUser);
+         
          // Figure Out What Pane to Add to
          System.out.println(newUser.stage);
          switch(newUser.stage) {
@@ -135,6 +143,7 @@ public class ScrumController implements Initializable  {
          		break;
          	default: //backlog
          		backLog.getChildren().add(newStory);
+         		break;
          }
          // add to stories
          if(this.stories != null) {
@@ -149,5 +158,257 @@ public class ScrumController implements Initializable  {
 	  Pane newLoadedPane =  FXMLLoader.load(getClass().getResource("story.fxml"));
 	  backLog.getChildren().add(newLoadedPane);
 	}
+	
+	@FXML
+	protected void toDoOnDragOver(DragEvent event) {
+		 if (event.getGestureSource() != toDo && event.getDragboard().hasString()) {
+		        event.acceptTransferModes(TransferMode.ANY);
+		 }
+		 event.consume();
+	}
+	
+	@FXML
+	protected void toDoOnDragDropped(DragEvent event) {
+		System.out.println("Drag dropped");
+	    //Get the drag-board back
+	    Dragboard db = event.getDragboard();
+	    boolean success = false;
+	    if (db.hasString()) {
+	    	String storyDataString = db.getString();
+	    	Properties props = new Properties();
+	    	try {
+				props.load(new StringReader(storyDataString.substring(1, storyDataString.length() - 1).replace(", ", "\n")));
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}       
+	    	Map<String, String> map2 = new HashMap<String, String>();
+	    	for (Map.Entry<Object, Object> e : props.entrySet()) {
+	    	    map2.put((String)e.getKey(), (String)e.getValue());
+	    	}
+	    	
+	    	// Place onto toDo Pane
+	        FXMLLoader loader2Temp = new FXMLLoader(getClass().getResource("story.fxml"));
+	        Pane newStoryTemp = null;
+	        try {
+				newStoryTemp = loader2Temp.load();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	        storyController storyControl = loader2Temp.getController();
+	        
+	        UserStory newUserTemp = new UserStory(map2.get("name"), map2.get("description"), "toDo", map2.get("priorityLevel"));
 
+	        storyControl.addStory(newUserTemp);
+	        
+	        toDo.getChildren().add(newStoryTemp);
+	        this.stories.addStoryWhole(newUserTemp);
+	        try {
+				this.gateway.updateStories(stories);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	    	
+	    	// Mark success
+	        success = true;
+	    }
+	    // Complete and consume the event.
+	    event.setDropCompleted(success);
+	    event.consume();
+	    
+	    // Remove child from original pane
+	    //toDo.getChildren().
+	}
+
+	@FXML
+	protected void inProgressDragOver(DragEvent event) {
+		 if (event.getGestureSource() != inProgress && event.getDragboard().hasString()) {
+		        event.acceptTransferModes(TransferMode.ANY);
+		 }
+		 event.consume();
+	}
+	
+	@FXML
+	protected void inProgressDragDropped(DragEvent event) {
+		System.out.println("Drag dropped");
+	    //Get the drag-board back
+	    Dragboard db = event.getDragboard();
+	    boolean success = false;
+	    if (db.hasString()) {
+	    	String storyDataString = db.getString();
+	    	Properties props = new Properties();
+	    	try {
+				props.load(new StringReader(storyDataString.substring(1, storyDataString.length() - 1).replace(", ", "\n")));
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}       
+	    	Map<String, String> map2 = new HashMap<String, String>();
+	    	for (Map.Entry<Object, Object> e : props.entrySet()) {
+	    	    map2.put((String)e.getKey(), (String)e.getValue());
+	    	}
+	    	
+	    	// Place onto toDo Pane
+	        FXMLLoader loader2Temp = new FXMLLoader(getClass().getResource("story.fxml"));
+	        Pane newStoryTemp = null;
+	        try {
+				newStoryTemp = loader2Temp.load();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	        storyController storyControl = loader2Temp.getController();
+	        
+	        UserStory newUserTemp = new UserStory(map2.get("name"), map2.get("description"), "inProgress", map2.get("priorityLevel"));
+
+	        storyControl.addStory(newUserTemp);
+	        
+	        inProgress.getChildren().add(newStoryTemp);
+	        this.stories.addStoryWhole(newUserTemp);
+	        try {
+				this.gateway.updateStories(stories);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	    	
+	    	// Mark success
+	        success = true;
+	    }
+	    // Complete and consume the event.
+	    event.setDropCompleted(success);
+	    event.consume();
+	    
+	    // Remove child from original pane
+	    //toDo.getChildren().
+	}
+	
+	@FXML
+	protected void completedDragOver(DragEvent event) {
+		 if (event.getGestureSource() != complete && event.getDragboard().hasString()) {
+		        event.acceptTransferModes(TransferMode.ANY);
+		 }
+		 event.consume();
+	}
+	
+	@FXML
+	protected void completedDragDropped(DragEvent event) {
+		System.out.println("Drag dropped");
+	    //Get the drag-board back
+	    Dragboard db = event.getDragboard();
+	    boolean success = false;
+	    if (db.hasString()) {
+	    	String storyDataString = db.getString();
+	    	Properties props = new Properties();
+	    	try {
+				props.load(new StringReader(storyDataString.substring(1, storyDataString.length() - 1).replace(", ", "\n")));
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}       
+	    	Map<String, String> map2 = new HashMap<String, String>();
+	    	for (Map.Entry<Object, Object> e : props.entrySet()) {
+	    	    map2.put((String)e.getKey(), (String)e.getValue());
+	    	}
+	    	
+	    	// Place onto toDo Pane
+	        FXMLLoader loader2Temp = new FXMLLoader(getClass().getResource("story.fxml"));
+	        Pane newStoryTemp = null;
+	        try {
+				newStoryTemp = loader2Temp.load();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	        storyController storyControl = loader2Temp.getController();
+	        
+	        UserStory newUserTemp = new UserStory(map2.get("name"), map2.get("description"), "complete", map2.get("priorityLevel"));
+
+	        storyControl.addStory(newUserTemp);
+	        
+	        complete.getChildren().add(newStoryTemp);
+	        this.stories.addStoryWhole(newUserTemp);
+	        try {
+				this.gateway.updateStories(stories);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	    	
+	    	// Mark success
+	        success = true;
+	    }
+	    // Complete and consume the event.
+	    event.setDropCompleted(success);
+	    event.consume();
+	    
+	    // Remove child from original pane
+	    //toDo.getChildren().
+	}
+	
+	@FXML
+	protected void backLogDragOver(DragEvent event) {
+		 if (event.getGestureSource() != backLog && event.getDragboard().hasString()) {
+		        event.acceptTransferModes(TransferMode.ANY);
+		 }
+		 event.consume();
+	}
+	
+	@FXML
+	protected void backLogDragDropped(DragEvent event) {
+		System.out.println("Drag dropped");
+	    //Get the drag-board back
+	    Dragboard db = event.getDragboard();
+	    boolean success = false;
+	    if (db.hasString()) {
+	    	String storyDataString = db.getString();
+	    	Properties props = new Properties();
+	    	try {
+				props.load(new StringReader(storyDataString.substring(1, storyDataString.length() - 1).replace(", ", "\n")));
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}       
+	    	Map<String, String> map2 = new HashMap<String, String>();
+	    	for (Map.Entry<Object, Object> e : props.entrySet()) {
+	    	    map2.put((String)e.getKey(), (String)e.getValue());
+	    	}
+	    	
+	    	// Place onto toDo Pane
+	        FXMLLoader loader2Temp = new FXMLLoader(getClass().getResource("story.fxml"));
+	        Pane newStoryTemp = null;
+	        try {
+				newStoryTemp = loader2Temp.load();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	        storyController storyControl = loader2Temp.getController();
+	        
+	        UserStory newUserTemp = new UserStory(map2.get("name"), map2.get("description"), "backLog", map2.get("priorityLevel"));
+
+	        storyControl.addStory(newUserTemp);
+	        
+	        backLog.getChildren().add(newStoryTemp);
+	        this.stories.addStoryWhole(newUserTemp);
+	        try {
+				this.gateway.updateStories(stories);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	    	
+	    	// Mark success
+	        success = true;
+	    }
+	    // Complete and consume the event.
+	    event.setDropCompleted(success);
+	    event.consume();
+	    
+	    // Remove child from original pane
+	    //toDo.getChildren().
+	}
+	
 }
--- ScrumBoard/src/client/storyController.java
+++ ScrumBoard/src/client/storyController.java
@@ -1,11 +1,23 @@
 package client;
 
+import java.util.HashMap;
+
 import application.UserStory;
+import client.StoryDataHelper;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.Pane;
 
 public class storyController {
 	
+	@FXML
+	private Pane storyPane;
 	@FXML
 	private Label name;
 	@FXML
@@ -29,6 +41,7 @@ public class storyController {
         priority.setText(string);
     }
 	
+	/*
 	public void openToEdit() {
 		//put info into edit pane on Scrum Board, pre populate, allow user to edit
 		
@@ -36,6 +49,36 @@ public class storyController {
 		
 		//update must be reflected in sories array and on board
 	}
+	*/
 	
+	@FXML
+	protected void storyOnDragDetected(MouseEvent event) {
+	    //We want the textArea to be dragged. Could also be copied.
+	    Dragboard db = storyPane.startDragAndDrop(TransferMode.MOVE);
+	    
+	    // Retrieve data from storyPane
+	    String tempName = ((Labeled) storyPane.getChildren().get(0)).getText();
+	    String tempDescription = ((Labeled) storyPane.getChildren().get(1)).getText();
+	    String tempPriorityLevel = ((Labeled) storyPane.getChildren().get(3)).getText();
+	    
+	    // Place string(s) data into JSON format
+	    HashMap<String, String> storyData = new HashMap<String, String>();
+	    storyData.put("name", tempName);
+	    storyData.put("description", tempDescription);
+	    storyData.put("priorityLevel", tempPriorityLevel);
+	    
+	    // Put a string on a drag-board as an identifier
+	    ClipboardContent content = new ClipboardContent();
+	    content.putString(storyData.toString());	    
+	    db.setContent(content);
+	    
+	    //Consume the event
+	    event.consume();
+	}
+	
+	@FXML
+	protected void storyOnDragDone(DragEvent event) {
+		System.out.println("Drag done");
+	}
 	
 }
