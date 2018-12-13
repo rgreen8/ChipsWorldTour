1c8f109da9d73237d21af860a0c5257fdb8b19f6
Ryan Green <rgreen8@nd.edu>
2018-12-13 09:30:27 -0500

    stuff

--- ScrumBoard/src/client/storyController.java
+++ ScrumBoard/src/client/storyController.java
@@ -1,8 +1,23 @@
 package client;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import application.UserStory;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class storyController {
 	
@@ -12,6 +27,10 @@ public class storyController {
 	private Label description;
 	@FXML
 	private Label priority;
+	@FXML
+	private ChoiceBox<String> choiceStage;
+	@FXML
+	private ChoiceBox<String> choicePriority;
 	
 	public void addStory(UserStory userStory) {
 		name.setText(userStory.name);
@@ -28,14 +47,20 @@ public class storyController {
 	public void setPriority(String string){
         priority.setText(string);
     }
-	
-	public void openToEdit() {
-		//put info into edit pane on Scrum Board, pre populate, allow user to edit
-		
-		//need a "update button" to hit if you want information
-		
-		//update must be reflected in sories array and on board
+	@FXML
+	private void openToEdit(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("storyEdit.fxml"));
+   	 	Parent root = loader.load();
+        Stage stage = new Stage ();
+        stage.setScene(new Scene(root));
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setTitle("New Story");
+        stage.showAndWait();
+        storyEditController storyEditController = loader.getController();
+        UserStory newUser = storyEditController.getUpdatedStory();
+        //added story is now in the main controller 
+
 	}
-	
 	
 }
